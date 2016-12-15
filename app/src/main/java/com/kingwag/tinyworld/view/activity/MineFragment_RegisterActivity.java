e3c8272d31036cf2555da1e80a27d53ee80240b4
ljxgenius <563619423@qq.com>
2016-12-15 20:15:34 +0800

    mine

---
+++ app/src/main/java/com/kingwag/tinyworld/view/activity/MineFragment_RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.kingwag.tinyworld.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kingwag.tinyworld.R;
+
+public class MineFragment_RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_fragment__register);
+
+    }
+}
--- app/src/main/java/com/kingwag/tinyworld/view/fragment/MineMainFragment.java
+++ app/src/main/java/com/kingwag/tinyworld/view/fragment/MineMainFragment.java
@@ -1,18 +1,25 @@
 package com.kingwag.tinyworld.view.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.kingwag.tinyworld.R;
+import com.kingwag.tinyworld.view.activity.MainActivity;
+import com.kingwag.tinyworld.view.activity.MineFragment_RegisterActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MineMainFragment extends Fragment {
+public class MineMainFragment extends Fragment implements View.OnClickListener{
+
+
 
 
     public MineMainFragment() {
@@ -25,6 +32,35 @@ public class MineMainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mine_main, container, false);
+
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //登录按钮
+        view.findViewById(R.id.btn_login).setOnClickListener(this);
+        //注册按钮
+        view.findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            //注册按钮跳转
+            case R.id.btn_register:
+                intent.setClass(getContext(), MineFragment_RegisterActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
 }
