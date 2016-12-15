0d5cd11af7e91ca7753f922f00692e5d2bd2fd0f
knoxyc <knoxyc@163.com>
2016-12-15 16:46:07 +0800

    购物车注释

--- app/src/main/java/com/kingwag/tinyworld/view/constants/ShopConstants.java
+++ app/src/main/java/com/kingwag/tinyworld/view/constants/ShopConstants.java
@@ -3,7 +3,7 @@ package com.kingwag.tinyworld.view.constants;
 /**
  * Created by kingwag on 2016/12/14.
  */
-
+//购物车登陆Key
 public class ShopConstants {
     public static final String SHOP_LOGIN_KEY = "loginState";
 }
