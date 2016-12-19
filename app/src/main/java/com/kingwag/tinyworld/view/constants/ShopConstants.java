b90ddd77de78c5c67d129c9a93ac7b0dcb749726
knoxyc <knoxyc@163.com>
2016-12-19 11:33:53 +0800

    加入日报

--- app/src/main/java/com/kingwag/tinyworld/view/constants/ShopConstants.java
+++ app/src/main/java/com/kingwag/tinyworld/view/constants/ShopConstants.java
@@ -3,7 +3,6 @@ package com.kingwag.tinyworld.view.constants;
 /**
  * Created by kingwag on 2016/12/14.
  */
-//ihikhk
 public class ShopConstants {
     public static final String SHOP_LOGIN_KEY = "loginState";
 }
